86a6cef3c6500b68b1a80ab143ae21e6d17365bf
Nick <59325182+Nick-B-Land@users.noreply.github.com>
2022-02-16 18:45:49 -0700

    Added missing employee controller methods (update and delete by id)

--- src/main/java/com/sait/ciaoToursEMS/controllers/EmployeeController.java
+++ src/main/java/com/sait/ciaoToursEMS/controllers/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.sait.ciaoToursEMS.controllers;
 
+import com.sait.ciaoToursEMS.exceptions.ResourceNotFoundException;
 import com.sait.ciaoToursEMS.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.sait.ciaoToursEMS.repositorys.EmployeeRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -25,6 +29,26 @@ public class EmployeeController {
     @PostMapping("/new-employee")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/update-employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No employee found with this id :" + id));
+
+        Employee updatedEmployee = employeeRepository.save(employeeDetails);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/delete-employee/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No employee found with this id  :" + id));
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/testAuth")
