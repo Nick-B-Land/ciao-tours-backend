786705685d90a9ded858fea16712e1b3b6cbfe3b
CyxDB <zyrislp@gmail.com>
2022-03-22 10:19:18 -0600

    Added data initialisation
    Added a roles.sql file that will insert roles on application startup. Also changed ddl-auto to create so that it drops/creates tables on application launch. This is useful for development but we shouldn't keep it like this. If you need to add any default table data then create an sql script for the table you want, and add inserts for all the data. Then you just need to add that file to spring.sql.init.data-locations. See  classpath:roles.sql for more info

---
+++ src/main/resources/roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO `roles` (`role_id`,`name`) VALUES (1,'ROLE_ADMIN');
+INSERT INTO `roles` (`role_id`,`name`) VALUES (2,'ROLE_BOOKKEEPER');
+INSERT INTO `roles` (`role_id`,`name`) VALUES (3,'ROLE_HR');
+INSERT INTO `roles` (`role_id`,`name`) VALUES (4,'ROLE_EMPLOYEE');
