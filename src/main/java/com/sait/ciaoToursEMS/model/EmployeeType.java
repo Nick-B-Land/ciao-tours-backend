7b7dbfc65a6113ce7d4ad8637c6fccebdfe949c4
Duncan Bain <zyrislp@gmail.com>
2022-04-07 14:59:50 -0600

    Update EmployeeType.java

--- src/main/java/com/sait/ciaoToursEMS/model/EmployeeType.java
+++ src/main/java/com/sait/ciaoToursEMS/model/EmployeeType.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * EmployeeType
+ *
+ * Represents the type of employee.
+ * 1. FULL-TIME
+ * 2. PART-TIME
+ * 3. ITALIAN
+ */
+
 @Entity
 public class EmployeeType {
 
@@ -47,18 +56,34 @@ public class EmployeeType {
         return employeeTypeId;
     }
 
+    /**
+     * Enum for the job category of the employee.
+     * @return EnumEmployeeTypes the job category of the employee.
+     */
     public EnumEmployeeTypes getDescription() {
         return description;
     }
 
+    /**
+     * Setter for the job category of the employee.
+     * @param description the job category of the employee.
+     */
     public void setDescription(EnumEmployeeTypes description) {
         this.description = description;
     }
 
+    /**
+     * Getter for the employees.
+     * @return List<Employee> the employees.
+     */
     public List<Employee> getEmployees() {
         return employees;
     }
 
+    /**
+     * Setter for the employees.
+     * @param employees the employees.
+     */
     public void setEmployees(List<Employee> employees) {
         this.employees = employees;
     }
